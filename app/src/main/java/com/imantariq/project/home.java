28f26f0853a9e677d6de96f6f7d3e40a55278bcd
imantariq <113774499+imantariq@users.noreply.github.com>
2022-11-13 17:19:30 +0500

    ingredients and directions done

--- app/src/main/java/com/imantariq/project/home.java
+++ app/src/main/java/com/imantariq/project/home.java
@@ -2,13 +2,24 @@ package com.imantariq.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class home extends AppCompatActivity {
-
+    ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        iv=findViewById(R.id.first);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(home.this,recipe.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/imantariq/project/nutrit.java
@@ -0,0 +1,14 @@
+package com.imantariq.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nutrit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nutrit);
+    }
+}
