1f39e7b84cb4ed6f9144d9fdf3de756f909671da
imantariq <113774499+imantariq@users.noreply.github.com>
2022-11-13 18:28:03 +0500

    shopping list dont

--- app/src/main/java/com/imantariq/project/shopping.java
+++ app/src/main/java/com/imantariq/project/shopping.java
@@ -2,13 +2,24 @@ package com.imantariq.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class shopping extends AppCompatActivity {
-
+    Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping);
+        bt=findViewById(R.id.back);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(shopping.this,recipe.class);
+                startActivity(intent);
+            }
+        });
     }
 }
