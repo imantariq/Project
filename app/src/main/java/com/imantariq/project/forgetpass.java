b2c546b0a8ba73a263c78dc5a72b483b0e648691
imantariq <113774499+imantariq@users.noreply.github.com>
2022-11-13 17:34:26 +0500

    nutrition done and forget pass updated

--- app/src/main/java/com/imantariq/project/forgetpass.java
+++ app/src/main/java/com/imantariq/project/forgetpass.java
@@ -1,14 +1,25 @@
 package com.imantariq.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class forgetpass extends AppCompatActivity {
-
+    Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgetpass);
+        bt=findViewById(R.id.done);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(forgetpass.this,login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
